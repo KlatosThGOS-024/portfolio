d4b5496463a5ca9f966eecffaa03fff44815a66d
KlatosThGOS-024 <priyanshutiwari878@yahoo.com>
2025-01-15 19:46:10 +0530

    responsiveness done

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,7 +1,46 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export const Education = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".schl",
+      {
+        opacity: 0,
+        x: -300,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 1.2,
+        scrollTrigger: {
+          trigger: ".schl",
+          start: "top 20%", // Starts animation when .schl is 80% in the viewport
+          end: "top 50%", // Ends animation when .schl is 20% out of the viewport
+          onEnter: () => {
+            gsap.to(".schl", { opacity: 1, x: 0, duration: 1.2 });
+          },
+          onLeave: () => {
+            gsap.to(".schl", { opacity: 0, x: -300, duration: 1.2 });
+          },
+          onEnterBack: () => {
+            gsap.to(".schl", { opacity: 1, x: 0, duration: 1.2 });
+          },
+          onLeaveBack: () => {
+            gsap.to(".schl", { opacity: 0, x: -300, duration: 1.2 });
+          },
+          scrub: true, // Ensures smooth transition between states
+        },
+      }
+    );
+  });
   return (
     <section className="xl:w-[1496px] mt-[64px]  mx-auto lg:w-[1028px] md:w-[960px]">
-      <div className="py-[96px]">
+      <div className="py-[96px] px-[64px]">
         <h2
           className="font-[700] mb-[96px] text-center text-white
          text-[48px]"
@@ -9,7 +48,7 @@ export const Education = () => {
           Education
         </h2>
         <div className=" space-y-[64px]">
-          <div className=" space-y-3">
+          <div className=" schl space-y-3">
             <p className="text-lg font-[600] text-[#FFC400]">2020-2022</p>
             <p
               className="font-[700] text-white
@@ -31,7 +70,7 @@ export const Education = () => {
             </span>
           </div>
           <hr className="  border-gray-500"></hr>
-          <div className=" space-y-3">
+          <div className=" clg space-y-3">
             <p className="text-lg font-[600] text-[#FFC400]">2023-2027</p>
             <p
               className="font-[700] text-white
@@ -52,10 +91,6 @@ export const Education = () => {
               Pursuing
             </span>
           </div>
-        </div>
-        <div>
-          <p></p>
-          <p></p>
         </div>
       </div>
     </section>
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -7,7 +7,10 @@ interface WorkType {
 
 function Work({ imgSrc, header, subHeader, technologies }: WorkType) {
   return (
-    <div className="my-9 work-item rounded-lg relative shadow-xl h-[620px] bg-[#424242]">
+    <div
+      className="my-9 work-item rounded-lg relative transition-all ease-in-out duration-500 hover:scale-110
+    shadow-xl h-[620px] bg-[#424242]"
+    >
       <div>
         <img
           className=" max-w-[100%] rounded-lg rounded-b-none "
@@ -15,14 +18,14 @@ function Work({ imgSrc, header, subHeader, technologies }: WorkType) {
           alt={header}
         />
         <div className="p-[28px]">
-          <h2 className=" text-[#FFC400] font-[700] text-[21px]">{header}</h2>
-          <p className="text-white text-[21px] font-[600] mt-3">{subHeader}</p>
-          <p className="text-center text-[#FFC400] text-[21px] font-[500]">
+          <h2 className=" text-[#FFC400] font-[500] text-[21px]">{header}</h2>
+          <p className="text-white text-[18px] font-[500] mt-3">{subHeader}</p>
+          <p className="text-center mt-[21px] text-[#FFC400] text-[21px] font-[500]">
             Techonologies
           </p>
-          <ul className=" flex space-x-6 text-[21px] mt-3 min-w-fit  flex-wrap">
+          <ul className=" flex text-[18px] gap-x-[18px] gap-y-[8px] mt-3 min-w-fit flex-wrap">
             {technologies.map((tech, index) => (
-              <li className=" text-white font-[600] " key={index}>
+              <li className=" font-[500] text-gray-300  " key={index}>
                 {tech}
               </li>
             ))}
@@ -63,22 +66,25 @@ function Work({ imgSrc, header, subHeader, technologies }: WorkType) {
 
 export const Works = () => {
   return (
-    <section className="xl:w-[1496px] mt-[64px]  mx-auto lg:w-[1028px] md:w-[960px]">
+    <section className="xl:w-[1496px] mt-[64px] px-[64px]  mx-auto lg:w-[1028px] md:w-[960px]">
       <div>
-        <h2 className=" text-white font-semibold  text-[48px] text-center">
+        <h2 className=" text-white px-[48px] font-semibold  text-[48px] text-center">
           Works
         </h2>
         <div>
           <div className=" flex gap-3  text-white text-[28px] items-center font-[600]">
             <span className="my-6">Selected Projects</span>
-            <span className="h-0.5 w-16 mt-3 bg-black dark:bg-white hidden md:block"></span>
+            <span className="h-0.5 w-16 mt-3 bg-black dark:bg-white md:block"></span>
           </div>
           <p className=" text-white text-[21px]">
             All small gallery of recent projects chosen by me. interrseted to
             see more? visit for more
           </p>
         </div>
-        <div className="grid grid-cols-3 gap-[28px]">
+        <div
+          className="grid max-lg:grid-cols-2 max-md:grid-cols-1 
+        grid-cols-3 gap-[28px]"
+        >
           <Work
             header="PromptPilot"
             subHeader="Bla blahBla blahBla blahBla blahBla blahBla blah"
